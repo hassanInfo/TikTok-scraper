21e428dc024fd43c16b71380e9eaa677c3b1b957
Hassan Id Mansour <85229840+hassanInfo@users.noreply.github.com>
2023-12-29 04:29:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def parse_arguments():
     - argparse.Namespace: Object containing parsed arguments
     """
     parser = argparse.ArgumentParser(description='TikTok Scraper')
-    parser.add_argument('--keywords', type=str, help='List of keywords to search on TikTok')
+    parser.add_argument('--keyword', type=str, help='keyword to search on TikTok')
     parser.add_argument('--n_post', type=int, default=300, help='Number of posts to scrape (default: 300)')
     parser.add_argument('--delay', type=int, default=10, help='Delay in seconds between processing steps (default: 10)')
 
@@ -20,8 +20,11 @@ if __name__ == '__main__':
     configure()
     # Parse command-line arguments
     args = parse_arguments()
-    # Create an instance of TikTokScraper
-    tiktok_scraper = TikTokScraper(keyword, args.n_post, args.delay)
-    tiktok_scraper.run_scraper()
-    # Save the collected data 
-    tiktok_scraper.save_data()
+    for k in keywords:
+        # Create an instance of TikTokScraper
+        tiktok_scraper = TikTokScraper(keyword, args.n_post, args.delay)
+        tiktok_scraper.run_scraper()
+        # Save the collected data 
+        tiktok_scraper.save_data()
+        
+    merge_csv_files('./data', 'tiktok_data')
